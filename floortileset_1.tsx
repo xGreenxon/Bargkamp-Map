a324c28e67c7f500ba020fcd8cbd792ac41796e6
Ruben <r_de_wall@web.de>
2021-03-18 00:57:43 +0100

    Update Alpha 1.0
    Pileset ausgewählt, Generelle Ideen gesammelt (SSstruktur). Grundrisse Rausgesucht

---
+++ floortileset_1.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="floortileset" tilewidth="32" tileheight="32" spacing="2" margin="1" tilecount="64" columns="8">
+ <image source="floortileset.png" width="272" height="272"/>
+ <tile id="37">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
