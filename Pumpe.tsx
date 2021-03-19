8d9481d3804a6b3ce6cfc0a45662dd48ac716015
Ruben <80848925+xGreenxon@users.noreply.github.com>
2021-03-19 03:03:01 +0100

    Add files via upload

---
+++ Pumpe.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Pumpe" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="Pumpe.png" width="32" height="32"/>
+</tileset>
---
+++ irgend_so_ein_ding_was_pfadfinder_benutzen_oder_so1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="irgend_so_ein_ding_was_pfadfinder_benutzen_oder_so1" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="irgend_so_ein_ding_was_pfadfinder_benutzen_oder_so1.png" width="128" height="128"/>
+</tileset>
